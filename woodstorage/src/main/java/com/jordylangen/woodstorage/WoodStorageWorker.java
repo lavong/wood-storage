7aa72da0836d6bba53723fb1344b4f9352eb2007
Jordy Langen <jordylangen@gmail.com>
2016-09-28 20:42:31 +0200

    cleanup & fixed a broken unittest

--- woodstorage/src/main/java/com/jordylangen/woodstorage/WoodStorageWorker.java
+++ woodstorage/src/main/java/com/jordylangen/woodstorage/WoodStorageWorker.java
@@ -1,7 +1,6 @@
 package com.jordylangen.woodstorage;
 
 import io.reactivex.Flowable;
-import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
